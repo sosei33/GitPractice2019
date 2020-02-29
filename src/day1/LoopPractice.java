00af77ee3866e00465fce2fe2751acdca6146c53
samuelosei <s.osei40@yahoo.com>
2020-02-29 15:55:30 -0500

    update

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -6,6 +6,8 @@ public class LoopPractice {
         //some random code
         for (int i = 0; i < 10; i+=2) {
             System.out.println("Counter : : " + 1);
+            System.out.println("########");
+            System.out.println("Java is Fun");
 
         }
     }
